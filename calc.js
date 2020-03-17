1cedb51855da9c94c3132517a51c03b6aa0c4bdd
Karolina Jasiak <kjasiak1993@gmail.com>
2020-03-18 00:48:13 +0100

    Initial commit

---
+++ calc.js
@@ -0,0 +1,19 @@
+function Calc() {
+    this.memory = 0; 
+ }
+ 
+ Calc.prototype.sum = (a, b) => {
+     return a + b;
+ }
+ 
+ Calc.prototype.substract = (a, b) => {
+     return a - b;
+ }
+ 
+ Calc.prototype.multiply = (a, b) => {
+     return a * b;
+ }
+ 
+ Calc.prototype.divide = (a, b) => {
+     return (b > 0) ? a / b : alert("Nie mozna dzielic przez zero!");
+ }
---
+++ script.js
@@ -0,0 +1,43 @@
+let calcWindow = document.querySelector(".calc-input-window input");
+let calcButtons = document.querySelector(".calc-input-buttons");
+
+function isCleared() {
+    return (calcWindow.value == 0);
+}
+
+function clear() {
+    console.log("clear()");
+    calcWindow.value = 0;
+}
+
+function separator() {
+    if (calcWindow.value.indexOf(",") >= 0)
+        return;
+    else
+        calcWindow.value += ",";
+}
+
+function getSepPosition() {
+    return (calcWindow.value.indexOf(",") > 0) ? calcWindow.value.length - calcWindow.value.indexOf(",") : -1;
+}
+
+
+calcButtons.addEventListener("click", (event) => {
+    let { target } = event;
+    console.log(target)
+
+    if (target.className.indexOf("btn") < 0)
+        return;
+
+    if (isCleared())
+        calcWindow.value = "";
+    
+    if (target.className.indexOf("btn-special") >= 0) {
+        let fnToRun = target.name;
+        window[fnToRun]();
+    }
+    else {
+        console.log(getSepPosition());
+        calcWindow.value = `${calcWindow.value}${target.innerText}`;
+    }
+});

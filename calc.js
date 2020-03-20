19e6c8dfa6c94a8f292df4ee601589940c0bf2b4
Karolina Jasiak <kjasiak1993@gmail.com>
2020-03-20 18:08:18 +0100

    Changed calculator formula

--- calc.js
+++ calc.js
@@ -2,7 +2,11 @@ function Calc() {
     this.memory = 0; 
     this.window = document.querySelector(".calc-input-window input");
     this.buttons = document.querySelector(".calc-input-buttons");
-    
+    this.operators = /[-|+|x|/]+/g;
+    this.numbers = /[0-9]+/g;
+    this.actions = /[C|=]+/g;
+    this.operand = null;
+    this.operator = null;
 };
  
  Calc.prototype.sum = function(a, b) {
@@ -21,46 +25,62 @@ function Calc() {
      return (b > 0) ? a / b : alert("Nie mozna dzielic przez zero!");
  }
 
- Calc.prototype.eval = function() {
-    let operands = this.getOperands();
-    let operators = this.window.value.match(/[+]+/g);
-
-    for (let i = 0; i < operands.length; i++)
-        operands[i] = operands[i].replace(',', '.');
-
-    if (operands == 0 || operators == null)
-        return;
-
-    let result = parseFloat(operands[0]);
-
-    for (let i = 1; i < operands.length; i++)
-        result += parseFloat(operands[i]);
-    
-    this.window.value = result.toString().replace('.',',');
- }
-
- Calc.prototype.getOperands = function() {
-    return this.window.value.split(/[+]+/);
- }
-
  Calc.prototype.isCleared = function() {
     return (this.window.value == 0);
 }
 
 Calc.prototype.clear = function() {
-    console.log("clear()");
     this.window.value = 0;
 }
 
-Calc.prototype.plus = function() {
-    if (this.window.value != 0)
-        this.window.value += "+";
+Calc.prototype.saveOperand = function(val) {
+    this.operand = val;
+}
+
+Calc.prototype.saveOperator = function(val) {
+    this.operator = val;
+}
+
+Calc.prototype.resetOperand = function() {
+    this.operand = null;
+}
+
+Calc.prototype.resetOperator = function() {
+    this.operator = null;
+}
+
+Calc.prototype.eval = function() {
+    let a = parseFloat(this.convertSep(this.operand));
+    let b = parseFloat(this.convertSep(this.window.value));
+    let result = 0;
+
+    switch(this.operator) {
+        case "+":
+            result = this.sum(a, b);
+            break;
+        case "-":
+            result = this.substract(a, b);
+            break;
+        case "x":
+            result = this.multiply(a, b);
+            break;
+        case "/":
+            result = this.divide(a, b);
+            break;
+    }
+    
+    this.resetOperand();
+    this.resetOperator();
+
+    this.window.value = result;
+}
+
+Calc.prototype.convertSep = function(val) {
+    return val.replace(',', '.');
 }
 
 Calc.prototype.separator = function() {
-    let operands = this.getOperands();
-
-    if (operands[operands.length-1].indexOf(",") >= 0)
+    if (this.window.value.indexOf(",") >= 0)
         return;
     else
         this.window.value += ",";
@@ -72,15 +92,24 @@ Calc.prototype.getSepPosition = function() {
 
 Calc.prototype.onButtonClick = function(event) {
     let { target } = event;
+    let btnText = target.innerHTML;
 
     if (target.className.indexOf("btn") < 0)
         return;
     
-    if (target.className.indexOf("btn-special") >= 0) {
+    if (btnText.match(this.operators)) {
+        if (this.operand === null) {
+            this.saveOperand(this.window.value);
+            this.clear();
+            this.saveOperator(btnText);
+        }
+    }
+    else if (btnText.match(this.actions)) {
+        console.log(target.name);
         let fnToRun = target.name;
         this[fnToRun]();
     }
-    else {
+    else if (btnText.match(this.numbers)) {
         if (this.isCleared())
             this.window.value = "";
 
